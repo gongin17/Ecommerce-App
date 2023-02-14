ca257967ee69995577f999be7245f579d34fbfd4
gongin17 <99765215+gongin17@users.noreply.github.com>
2023-02-14 19:15:22 +0100

    Update getStripe.js

--- client/src/Service/getStripe.js
+++ client/src/Service/getStripe.js
@@ -4,7 +4,7 @@ let stripePromise;
 
 const getStripe = () => {
   if(!stripePromise) {
-    stripePromise = loadStripe("pk_test_51HN1SDDnSY2xnLBJBLVpQ4bWKaMHt53n4RLac8i0ikSWvIZGGfjTbFpn7eAAoIwl7p8aWqYn6ER8VjvDfj5pgJxL00DEeyECfa");
+    stripePromise = loadStripe("pk_test");
   }
 
   return stripePromise;
